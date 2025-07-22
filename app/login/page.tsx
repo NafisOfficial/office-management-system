74a00d7ba732719f25fcdc75df737ba0581247f8
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2025-07-22 18:53:11 +0600

    unfinish

---
+++ app/login/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React from "react";
+
+const login = () => {
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        const email = (e.target as HTMLFormElement).email.value;
+        const password = (e.target as HTMLFormElement).password.value;
+        console.log("Email:", email, "Password:", password);
+    };
+
+
+  return (
+    <div className="border p-5 max-w-md mx-auto mt-10 bg-white shadow-md rounded-lg">
+        <h1 className="text-2xl font-bold text-center mt-10 ">Login Page</h1>
+        <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-5">
+            <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+            <input type="email" id="email" className="mt-1 block w-full border-gray-600 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500" required />
+            </div>
+            <div className="mb-4">
+            <label htmlFor="password" className="block text-sm font-medium text-gray-600">Password</label>
+            <input type="password" id="password" className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500" required />
+            </div>
+            <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600">Login</button>
+        </form>
+    </div>
+  );
+};
+
+export default login;
