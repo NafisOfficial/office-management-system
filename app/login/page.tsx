930aaf40d0886587913c8ab8918163f7205d5b96
Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2025-07-23 06:05:28 +0600

    bug fixed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,26 +1,38 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 
 const login = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const target = e.target as typeof e.target & {
+      name: { value: string };
+      email: { value: string };
+      password: { value: string };
+    };
+
+    const name = target.name.value;
+    const email = target.email.value;
+    const password = target.password.value;
+
+    console.log({ name, email, password });
+  };
+
   return (
     <div className="flex justify-center items-center h-screen ">
       <Card className="w-full max-w-sm">
         <div className="flex justify-between items-center mx-6">
           <p className="text-2xl">Login to your account</p>
           <div>
-            <Link href="/register" className="text-blue-700">Sign up</Link>
+            <Link href="/register" className="text-blue-700">
+              Sign up
+            </Link>
           </div>
         </div>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="flex flex-col gap-6">
               <div className="grid gap-2">
                 <Label htmlFor="email">Email</Label>
@@ -41,16 +53,24 @@ const login = () => {
                     Forgot your password?
                   </a>
                 </div>
-                <Input id="password" type="password" required />
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="password"
+                  required
+                />
               </div>
             </div>
           </form>
         </CardContent>
         <CardFooter className="flex-col gap-2">
-          <Button type="submit" className="w-full bg-blue-700 hover:bg-blue-800 text-white">
+          <Button
+            type="submit"
+            className="w-full bg-blue-700 hover:bg-blue-800 text-white"
+          >
             Log in
           </Button>
-          <Button variant="outline" className="w-full"> 
+          <Button variant="outline" className="w-full">
             Log in with Google
           </Button>
         </CardFooter>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,40 +1,34 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 
 const register = () => {
-
-
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     const target = e.target as typeof e.target & {
-      name: { value: string };
       email: { value: string };
       password: { value: string };
     };
-
-    const name = target.name.value;
     const email = target.email.value;
     const password = target.password.value;
-
-    console.log({ name, email, password });
+    console.log({ email, password });
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex justify-center items-center h-screen ">
+    <form
+      onSubmit={handleSubmit}
+      className="flex justify-center items-center h-screen "
+    >
       <Card className="w-full max-w-sm">
         <div className="flex justify-between items-center mx-6">
           <p className="text-2xl">Register a account</p>
           <div>
-            <Link href="/login" className="text-blue-700">Log in</Link>
+            <Link href="/login" className="text-blue-700">
+              Log in
+            </Link>
           </div>
         </div>
         <CardContent>
@@ -42,12 +36,7 @@ const register = () => {
             <div className="flex flex-col gap-6">
               <div className="grid gap-2">
                 <Label htmlFor="email">Name</Label>
-                <Input
-                  id="Name"
-                  type="text"
-                  placeholder="name"
-                  required
-                />
+                <Input id="Name" type="text" placeholder="name" required />
               </div>
               <div className="grid gap-2">
                 <Label htmlFor="email">Email</Label>
@@ -62,16 +51,24 @@ const register = () => {
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
                 </div>
-                <Input id="password" type="password" placeholder="password" required />
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="password"
+                  required
+                />
               </div>
             </div>
           </form>
         </CardContent>
         <CardFooter className="flex-col gap-2">
-          <Button type="submit" className="w-full bg-blue-700 hover:bg-blue-800 text-white">
+          <Button
+            type="submit"
+            className="w-full bg-blue-700 hover:bg-blue-800 text-white"
+          >
             Sign up
           </Button>
-          <Button variant="outline" className="w-full"> 
+          <Button variant="outline" className="w-full">
             Sign up with Google
           </Button>
         </CardFooter>
